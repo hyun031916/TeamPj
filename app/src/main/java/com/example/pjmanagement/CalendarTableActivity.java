c0d3a0f5cbdffebe970d355e0d7e1d841d38f3cd
ksh <hyun031916@gmail.com>
2020-10-21 22:19:14 +0900

    #16 캘린더 다시 구현

---
+++ app/src/main/java/com/example/pjmanagement/CalendarTableActivity.java
@@ -0,0 +1,110 @@
+package com.example.pjmanagement;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.OrientationHelper;
+
+import com.applikeysolutions.cosmocalendar.utils.SelectionType;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class CalendarTableActivity extends AppCompatActivity  implements RadioGroup.OnCheckedChangeListener{
+
+    private CalendarView calendarView;
+    private Bundle savedInstanceState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_table);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        initViews();
+    }
+    private void initViews() {
+        calendarView = (CalendarView) findViewById(R.id.calendar_view);
+        calendarView.setCalendarOrientation(OrientationHelper.HORIZONTAL);
+
+        ((RadioGroup) findViewById(R.id.rg_selection_type)).setOnCheckedChangeListener(this);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.clear_selections:
+                clearSelectionsMenuClick();
+                return true;
+
+            case R.id.show_selections:
+                List<Calendar> days = calendarView.getSelectedDates();
+
+                String result="";
+                for( int i=0; i<days.size(); i++)
+                {
+                    Calendar calendar = days.get(i);
+                    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+                    final int month = calendar.get(Calendar.MONTH);
+                    final int year = calendar.get(Calendar.YEAR);
+                    String week = new SimpleDateFormat("EE").format(calendar.getTime());
+                    String day_full = year + "년 "+ (month+1)  + "월 " + day + "일 " + week + "요일";
+                    result += (day_full + "\n");
+                }
+                Toast.makeText(CalendarTableActivity.this, result, Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
+    private void clearSelectionsMenuClick() {
+        calendarView.clearSelections();
+
+    }
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+
+            case R.id.rb_single:
+                calendarView.setSelectionType(SelectionType.SINGLE);
+                break;
+
+            case R.id.rb_multiple:
+                calendarView.setSelectionType(SelectionType.MULTIPLE);
+                break;
+
+            case R.id.rb_range:
+                calendarView.setSelectionType(SelectionType.RANGE);
+                break;
+
+            case R.id.rb_none:
+                calendarView.setSelectionType(SelectionType.NONE);
+                break;
+        }
+    }
+}
